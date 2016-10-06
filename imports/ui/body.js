db82cc9dce9000376d7fbb7d80e5010697c540ce
Ashley Hsiao <aiyihsiao@gmail.com>
2016-10-05 22:40:10 -0700

    Add event handler for form submit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -8,5 +8,26 @@ import './body.html';
 Template.body.helpers({
   tasks() {
     return Tasks.find({})
-  }
-})
+  },
+});
+
+// Add event handler to listen to the submit event on the form
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;  // this grabs the whole form that has event listener on it
+    const text = target.text.value;  // this grabs the value from element with 'text' tagname property
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(),  // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+});
