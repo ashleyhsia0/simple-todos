439bb6ebe4d8cc9a8858ecf91def84a3c1fe0bdb
Ashley Hsiao <aiyihsiao@gmail.com>
2016-10-06 23:32:28 -0700

    Add comments

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,10 +6,13 @@ import { Tasks } from '../api/tasks.js';
 import './task.js';
 import './body.html';
 
+// ReactiveDict to store temporary reactive state on the client
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
 });
 
+// Helper is something that we will have access to in our html
+// it can be things that are processed by JavaScript, items that are grabbed from the database, etc.
 Template.body.helpers({
   // Get tasks from a collection instead of a static array
   tasks() {
@@ -23,6 +26,8 @@ Template.body.helpers({
   },
 });
 
+// Event, just like Helper, is a method that accepts an object
+// Properties are always strings - contains the event that we are looking for and the item that we are looking for, for that event to take place on, with a function as the value
 Template.body.events({
   // Add event handler to listen to the submit event on the form
   'submit .new-task'(event) {
@@ -42,6 +47,8 @@ Template.body.events({
     // Clear form
     target.text.value = '';
   },
+  // Add event handler to update ReactiveDict variable when the checkbox is checked or unchecked
+  // Event handler takes two arguments, the second of which is the same template instance which was this in the onCreated callback
   'change .hide-completed input'(event, instance) {
     instance.state.set('hideCompleted', event.target.checked);
   },
